96fc1e3bcbaade1fc571cc76b7716fd81091bed6
MilosM33 <milosmarejka89@gmail.com>
2020-10-08 13:50:04 +0200

    init

--- Open-Lab-03.03/Comparator.cs
+++ Open-Lab-03.03/Comparator.cs
@@ -4,9 +4,7 @@ namespace Open_Lab_03._03
 {
     public class Comparator
     {
-        public bool CompareCharactersCount(string str1, string str2)
-        {
-            throw new NotImplementedException();
-        }
+        public bool CompareCharactersCount(string str1, string str2) => str1.Length == str2.Length;
+       
     }
 }
